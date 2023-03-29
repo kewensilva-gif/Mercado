b51cdbf499d4329fa54fb5f8015ecdee523f3d2b
Kewen Silva <silvakewen52@gmail.com>
2023-03-29 17:08:22 -0300

    inserindo gerador de json

--- main.cpp
+++ main.cpp
@@ -36,33 +36,119 @@ void insereProduto(){
     produtos.close();
 }
 
-void lerProduto(){
+
+
+void produtoJson(){
     fstream produtos;
     string arquivo;
     string linha;
-    produtos.open("produtos.txt", ios::in);
+    produtos.open("produtos.json", ios::in);
 
     if(produtos.is_open()){
         while(getline(produtos, linha)){
-            if(linha == "id: 3"){
+            if(linha == "\t]"){
                 continue;
             } else
                 arquivo += (linha + "\n");
         }
-        cout << arquivo << endl;
+    cout << arquivo << endl;
     } else {
         cout << "Não foi possivel abrir o arquivo" << endl;
     }
 
     produtos.close();
 
-    produtos.open("produtos.txt", ios::out);
+    produtos.open("produtos.json", ios::out);
 
     produtos << arquivo;
 
     produtos.close();
 
 }
+
+void criarListaJson(){
+    char nome[30];
+    int quant;
+    char marca[30];
+    int id;
+    int sair;
+
+    fstream produtos;
+    produtos.open("produtos.json", ios::app);
+    produtos << "{\n\t\"produtos\": [ " << endl;
+     do{
+        cout << "Digite o nome do produto: ";
+        cin >> nome;
+        cout << "Digite a quantidade: ";
+        cin >> quant;
+        cout << "Digite a marca: ";
+        cin >> marca;
+        cout << "Digite o id: ";
+        cin >> id;
+
+        produtos << "\t\t{" << endl;
+        produtos << "\t\t\t\"Nome\": \"" << nome << "\","<< endl;
+        produtos << "\t\t\t\"Quantidade\": " << quant << ","<< endl;
+        produtos << "\t\t\t\"Marca\": \"" << marca << "\","<< endl;
+        produtos << "\t\t\t\"id\": " << id << endl;
+        produtos << "\t\t}\t" << endl;
+
+        cout << "Digite 0 para sair: ";
+        cin >> sair;
+    } while(sair);
+
+    produtos << "\t]\n}" << endl;
+    produtos.close();
+}
+
+insereNovoProduto(){
+    char nome[30];
+    int quant;
+    char marca[30];
+    int id;
+    fstream produtos;
+    string arquivo;
+    string linha;
+    produtos.open("produtos.json", ios::in);
+
+    if(produtos.is_open()){
+        while(getline(produtos, linha)){
+            if(linha == "\t\t}\t"){
+                break;
+            } else
+                arquivo += (linha + "\n");
+        }
+    cout << arquivo << endl;
+    } else {
+        cout << "Não foi possivel abrir o arquivo" << endl;
+    }
+
+    produtos.close();
+
+    produtos.open("produtos.json", ios::out);
+
+    produtos << arquivo;
+    produtos << "\t\t},\t" << endl;
+    produtos << "\t\t{" << endl;
+    cout << "Digite o nome do produto: ";
+    cin >> nome;
+    cout << "Digite a quantidade: ";
+    cin >> quant;
+    cout << "Digite a marca: ";
+    cin >> marca;
+    cout << "Digite o id: ";
+    cin >> id;
+    produtos << "\t\t\t\"Nome\": \"" << nome << "\","<< endl;
+    produtos << "\t\t\t\"Quantidade\": " << quant << ","<< endl;
+    produtos << "\t\t\t\"Marca\": \"" << marca << "\","<< endl;
+    produtos << "\t\t\t\"id\": " << id << endl;
+    produtos << "\t\t}\t" << endl;
+    produtos << "\t]\n}" << endl;
+
+    produtos.close();
+
+}
+
 int main()
 {
     int op, posicao = 0, pos;
@@ -75,12 +161,14 @@ int main()
         switch(op){
 
         case 1:
-            insereProduto();
+            //insereProduto();
+            criarListaJson();
             //cadastrarProduto(produtos, posicao);
             break;
 
         case 2:
-            lerProduto();
+            produtoJson();
+            //lerProduto();
             //listarProdutos(produtos, posicao);
             break;
 
@@ -94,6 +182,9 @@ int main()
             cout << "Digite o id do produto: ";
             cin >> pos;
             adicionarQuant(produtos, pos);
+            break;
+        case 5:
+            insereNovoProduto();
             break;
         }
     } while(op);
